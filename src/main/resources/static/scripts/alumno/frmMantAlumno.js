006621392fdf6336d37a18f1acb7743041485ea8
MarcoAranda <umauricio2705@gmail.com>
2023-05-07 09:59:30 -0500

    Funcionando ambos listados

---
+++ src/main/resources/static/scripts/alumno/frmMantAlumno.js
@@ -0,0 +1,118 @@
+$(document).on("click", "#btnagregar", function(){
+   $("#txtidalumno").val("A");
+    $("#txtapelalumno").val("");
+    $("#txtnomalumno").val("");
+    $("#cboesp").empty();
+    $("#txtproce").val("N")
+    $.ajax({
+        type: "GET",
+        url: "/Especialidad/listarEspecialidad",
+        dataType: "json",
+        success: function(resultado){
+            $.each(resultado, function(index, value){
+                $("#cboesp").append(
+                    `<option value="${value.idesp}">${value.nomesp}</option>`
+                )
+            })
+        }
+    })
+    $("#modalNuevo").modal("show");
+});
+
+$(document).on("click", ".btnactualizar", function(){
+    $("#txtidalumno").val($(this).attr("data-idalumno"));
+    $("#txtapealumno").val($(this).attr("data-apealumno"));
+    $("#txtnomalumno").val($(this).attr("data-nomalumno"));
+    $("#cboesp").empty();
+    var idesp = $(this).attr("data-idesp");
+     $("#txtproce").val($(this).attr("data-proce"));
+
+    $.ajax({
+        type: "GET",
+        url: "/Especialidad/listarEspecialidad",
+        dataType: "json",
+        success: function(resultado){
+            $.each(resultado, function(index, value){
+                $("#cboesp").append(
+                    `<option value="${value.idesp}">${value.nomesp}</option>`
+                )
+            })
+            $("#cboesp").val(idesp);
+        }
+    })
+    $("#modalNuevo").modal("show");
+});
+
+
+$(document).on("click", "#btnguardar", function(){
+    $.ajax({
+        type: "POST",
+        url: "/Alumno/registrarAlumno",
+        contentType: "application/json",
+        data: JSON.stringify({
+            idalumno: $("#txtidalumno").val(),
+            apealumno: $("#txtapealumno").val(),
+            nomalumno: $("#txtnomalumno").val(),
+            idesp: $("#cboesp").val()
+            proce:$("#txtproce").val()
+        }),
+        success: function(resultado){
+            if(resultado.respuesta){
+                ListarAlumno();
+            }
+            alert(resultado.mensaje);
+        }
+    });
+    $("#modalNuevo").modal("hide");
+});
+
+$(document).on("click", ".btneliminar", function(){
+    var idsala = $(this).attr("data-idsala");
+    $.ajax({
+        type: "DELETE",
+        url: "/Alumno/eliminarAlumno",
+        contentType: "application/json",
+        data: JSON.stringify({
+            idalumno: idalumno
+        }),
+        success: function(resultado){
+            if(resultado.respuesta){
+                ListarAlumno();
+            }
+            alert(resultado.mensaje);
+        }
+    });
+});
+
+function ListarAlumno(){
+    $.ajax({
+        type: "GET",
+        url: "/Alumno/listarAlumnos",
+        dataType: "json",
+        success: function(resultado){
+            $("#tblalumno > tbody").html("");
+            $.each(resultado, function(index, value){
+                $("#tblalumno > tbody").append("<tr>"+
+                    "<td>"+value.idalumno+"</td>"+
+                    "<td>"+value.apealumno+"</td>"+
+                    "<td>"+value.nomalumno+"</td>"+
+                     "<td>"+value.nomesp+"</td>"+
+                      "<td>"+value.proce+"</td>"+
+                    "<td>"+
+                        "<button type='button' class='btn btn-info btnactualizar'"+
+                                     "data-idalumno='"+value.idalumno+"'"+
+                                     "data-apealumno='"+value.apealumno+"'"+
+                                     "data-nomalumno='"+value.nomalumno+"'"+
+                                      "data-asientos='"+value.nomalumno+"'"+
+                                       "data-nomesp='"+value.nomesp+"'"+
+                                     "data-proce='"+value.proce+"'>Actualizar</button>"+
+                    "</td>"+
+                    "<td>"+
+                        "<button type='button' class='btn btn-danger btneliminar'"+
+                                     "data-idalumno='"+value.idalumno+"'"+
+                                     "data-apealumno='"+value.apealumno+"'>Eliminar</button>"+
+                    "</td></tr>");
+            })
+        }
+    })
+}
