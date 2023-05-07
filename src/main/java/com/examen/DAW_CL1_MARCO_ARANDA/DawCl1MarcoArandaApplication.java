64add57d7e27f26b4dc90909ca4cd4aa0e931c85
MarcoAranda <umauricio2705@gmail.com>
2023-05-07 08:47:50 -0500

    Initial commit

---
+++ src/main/java/com/examen/DAW_CL1_MARCO_ARANDA/DawCl1MarcoArandaApplication.java
@@ -0,0 +1,13 @@
+package com.examen.DAW_CL1_MARCO_ARANDA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawCl1MarcoArandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawCl1MarcoArandaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examen/DAW_CL1_MARCO_ARANDA/DawCl1MarcoArandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.DAW_CL1_MARCO_ARANDA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawCl1MarcoArandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
